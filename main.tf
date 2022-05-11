d49cb68eb036dee1ef3b6210d74917df613ff8dd
yashu1506 <98607047+yashu1506@users.noreply.github.com>
2022-05-11 17:19:23 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "us-east-1"
+}
+module "my_vpc" {
+    source = "../modules/vpc"
+    vpc_cidr = "192.168.0.0/16"
+    tenancy = "default"
+    vpc_id = "${module.my_vpc.vpc_id}"
+    subnet_cidr = "192.168.1.0/24"
+}
+
+module "my_ec2" {
+    source = "../modules/ec2"
+    ec2_count = 1
+    ami_id = "ami-09d56f8956ab235b3"
+    instance_type = "t2.micro"
+    subnet_id = "${module.my_vpc.subnet_id}"
+}
