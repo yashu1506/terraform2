81f6ef5201c6d0ead4b56e9171c308310594356d
yashu1506 <98607047+yashu1506@users.noreply.github.com>
2022-05-12 10:56:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAZSBWQT4VQTR432GC"
-    secret_key = "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"
+    access_key = "AKIA4CWKJZPJJ3CIKOKY"
+    secret_key = "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"
 }
 module "vpc_1" {
-    source = "./modules/vpc"
+    source = "../modules/vpc"
     vpc_cidr = "192.162.0.0/16"
     tenancy = "default"
     vpc_id = "${module.vpc_1.vpc_id}"
@@ -12,7 +12,7 @@ module "vpc_1" {
 }
 
 module "ec2_1" {
-    source = "./modules/ec2"
+    source = "../modules/ec2"
     ec2_count = 1
     ami_id = "ami-09d56f8956ab235b3"
     instance_type = "t2.micro"
