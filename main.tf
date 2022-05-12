fa5c32b932f62e07a4bfa7cc1d148767e6b56e4a
yashu1506 <98607047+yashu1506@users.noreply.github.com>
2022-05-12 10:58:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
     secret_key = "NzMHgvc4AwHiUpt8ibjBIOD5hnPUHGDNT+fxxwL3"
 }
 module "my_vpc" {
-    source = "../modules/vpc"
+    source = "./modules/vpc"
     vpc_cidr = "192.168.0.0/16"
     tenancy = "default"
     vpc_id = "${module.my_vpc.vpc_id}"
@@ -12,7 +12,7 @@ module "my_vpc" {
 }
 
 module "my_ec2" {
-    source = "../modules/ec2"
+    source = "./modules/ec2"
     ec2_count = 1
     ami_id = "ami-0c4f7023847b90238"
     instance_type = "t2.micro"
